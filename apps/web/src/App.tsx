f8fbe0cf090805434d9db5f851f04afad49382ec
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-07-13 22:03:59 +0200

    feat: wrapping in Page component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,25 +5,33 @@ import { useModelStore } from './store/useModelStore';
 import { ExampleDataType, getRenderAndUpdateGraphForReferencableDataType } from 'orm';
 import { FlatTreeRender } from './components/data/content/FlatTreeRenderer';
 import { Loading } from './components/data/content/Loading';
+import { Page } from './components/general/Page';
 
 const objectsToRender = getRenderAndUpdateGraphForReferencableDataType<DatabaseType>(ExampleDataType);
 
 export const App: React.FC = () => {
-  const data = useModelStore(s => s.data);
-  const loaded = useModelStore(s => s.loaded);
+  const data = useModelStore((s) => s.data);
+  const loaded = useModelStore((s) => s.loaded);
 
   useEffect(() => {
-    useModelStore.getState().getAllData()
+    useModelStore.getState().getAllData();
   }, []);
 
   return (
-    loaded ? 
-    <div>
-      <div>
-        <span className="flex flex-row gap-2 p-2 text-black">{helperMethod('helper!')}</span>
-        <div className="max-h-[85svh] overflow-y-scroll">else</div>
-        {(data?.components && data?.components[0]) ? <FlatTreeRender o={data.components[0]} dataDefinition={objectsToRender['ComponentType']} /> : null}
-      </div>
-    </div> : <Loading />
+    <Page>
+      {loaded ? (
+        <div>
+          <div>
+            <span className="flex flex-row gap-2 p-2 text-black">{helperMethod('helper!')}</span>
+            <div className="max-h-[85svh] overflow-y-scroll">else</div>
+            {data?.components && data?.components[0] ? (
+              <FlatTreeRender o={data.components[0]} dataDefinition={objectsToRender['ComponentType']} />
+            ) : null}
+          </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </Page>
   );
 };
--- apps/web/src/components/data/NestedDataForObjectRenderer.tsx
+++ apps/web/src/components/data/NestedDataForObjectRenderer.tsx
@@ -5,6 +5,7 @@ import { DatabaseType } from 'core';
 import { MissingObject } from './content/MissingObject';
 import { CollapsableRenderer } from './content/CollapsableRenderer';
 import { Loading } from './content/Loading';
+import { Page } from '../general/Page';
 
 export const NestedDataForObjectRenderer: React.FC = () => {
   const { collectionName, id } = useParams();
@@ -15,15 +16,19 @@ export const NestedDataForObjectRenderer: React.FC = () => {
     if (Object.keys(data).length === 0) useModelStore.getState().getAllData();
   }, [data]);
 
-  return loaded ? (
-    collectionName && id ? (
-      <div className="grid grid-cols-[100px_auto] gap-2 p-2 bg-zinc-200">
-        <CollapsableRenderer collectionName={collectionName as keyof DatabaseType} id={id} />
-      </div>
-    ) : (
-      <MissingObject collectionName={collectionName} id={id} renderStyle="full-page" />
-    )
-  ) : (
-    <Loading />
+  return (
+    <Page>
+      {loaded ? (
+        collectionName && id ? (
+          <div className="grid grid-cols-[100px_auto] gap-2 p-2 bg-zinc-200">
+            <CollapsableRenderer collectionName={collectionName as keyof DatabaseType} id={id} />
+          </div>
+        ) : (
+          <MissingObject collectionName={collectionName} id={id} renderStyle="full-page" />
+        )
+      ) : (
+        <Loading />
+      )}
+    </Page>
   );
 };
