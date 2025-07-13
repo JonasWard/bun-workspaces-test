ba3ae8a3d26fe47dc8c7f548e0c54ff8a1f4ea51
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-07-13 18:58:06 +0200

    feat: enable not using elysia in backend to use dumb database

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,14 @@
 import { Elysia } from 'elysia';
 import { cors } from '@elysiajs/cors';
-import { ExampleDataType } from 'orm';
+import { ExampleDataType, registerRoutersOnAppForDummyData } from 'orm';
 import { connectToDatabase } from './database';
 import swagger from '@elysiajs/swagger';
 import { registerRoutersOnAppForMongoDB } from './mongo/createMongoCalls';
+import { exampleData } from 'core';
 
-const db = await connectToDatabase();
+let db: any;
+
+if (process.env.NO_DATABASE !== 'true') db = await connectToDatabase();
 
 const app = new Elysia().use(cors()).listen(process.env.PORT || 5000);
 
@@ -22,6 +25,8 @@ if (process.env.SWAGGER === 'true')
     })
   );
 
-registerRoutersOnAppForMongoDB(app as any, ExampleDataType, db);
+if (process.env.NO_DATABASE !== 'true') registerRoutersOnAppForMongoDB(app as any, ExampleDataType, db);
+if (process.env.NO_DATABASE === 'true')
+  registerRoutersOnAppForDummyData(app as any, ExampleDataType, { ...exampleData });
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
