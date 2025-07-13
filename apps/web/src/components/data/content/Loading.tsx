ac2206471193003bb4c32326cf8cad9f235033d2
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-07-13 22:02:56 +0200

    feat: description text on why loading can take some time

--- apps/web/src/components/data/content/Loading.tsx
+++ apps/web/src/components/data/content/Loading.tsx
@@ -6,6 +6,9 @@ export const Loading: React.FC<{ optionalString?: string }> = ({ optionalString 
   return (
     <div className="flex flex-col min-h-screen justify-center items-center bg-slate-200">
       <div className="flex flex-col bg-slate-300 p-3 gap-3 text-slate-700 items-center rounded-lg border-4 border-solid border-slate-100 drop-shadow-lg">
+        <span>{`backend is hosted on Render.com free tier`}</span>
+        <span>{`which spins down after 15 minutes of inactivity`}</span>
+        <span>{`it can take half a minute for the backend to spin up!`}</span>
         <span>{`loaded ${loadedCollections.length} collections`}</span>
         <span>{optionalString ?? 'There is nothing to be seen yet!'}</span>
       </div>
